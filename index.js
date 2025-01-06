ec039271bf80e6b0780f48abcf7956a44b734575
Fampysek <kfampy@gmail.com>
2025-01-06 16:56:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🟢 Playing Queryex Services ", "🎮 Playing VALORANT"];
+const statusMessages = ["🟢 listening Queryex Services ", "🟢 watching Queryex Ticket "];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
