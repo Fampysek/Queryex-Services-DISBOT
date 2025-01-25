2fafc9d1be1b597fa0834c6d2f6f687922637860
Fampysek <kfampy@gmail.com>
2025-01-25 12:23:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = [" listening Queryex Services ", " watching Queryex Ticket "];
+const statusMessages = [" Chilling https://www.twitch.tv/lookinsomething ", " Chilling https://www.twitch.tv/lookinsomething "];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
